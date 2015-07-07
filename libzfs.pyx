528ac179a5a26a4d2acf7d30161e7feb0f8682d7
Baptiste Daroussin <baptiste.daroussin@gandi.net>
2015-07-07 15:34:32 +0200

    Allow to create a zvol

--- libzfs.pyx
+++ libzfs.pyx
@@ -736,6 +736,15 @@ cdef class ZFSPool(object):
             <nvpair.nvlist_t*>cfsopts) != 0:
             raise ZFSException(self.root.errno, self.root.errstr)
 
+    def createvol(self, name, fsopts):
+        cdef uintptr_t cfsopts = fsopts.handle()
+        if libzfs.zfs_create(
+            <libzfs.libzfs_handle_t*>self.root.handle(),
+            name,
+            zfs.ZFS_TYPE_VOLUME,
+            <nvpair.nvlist_t*>cfsopts) != 0:
+            raise ZFSException(self.root.errno, self.root.errstr)
+
     def destroy(self, name):
         pass
 
