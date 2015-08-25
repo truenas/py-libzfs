b13131bda8850d1956662a1f3928805bc7db388b
Jakub Klama <jakub@ixsystems.com>
2015-08-25 15:51:14 +0200

    Fix missing cdef.

--- libzfs.pyx
+++ libzfs.pyx
@@ -240,6 +240,7 @@ cdef class ZFS(object):
         )
 
     cdef ZFSVdev make_vdev_tree(self, topology):
+        cdef ZFSVdev root
         print dict(topology)
         root = ZFSVdev(self, 'root')
         root.children = topology.get('data', [])
