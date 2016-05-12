b6d3ccee5b84609de49fc28bfa433d6745577485
Jakub Klama <jakub@ixsystems.com>
2016-05-12 15:46:57 +0200

    Fix typo/thinko

--- libzfs.pyx
+++ libzfs.pyx
@@ -297,7 +297,7 @@ cdef class ZFS(object):
             root.nvlist['l2cache'] = [(<ZFSVdev>i).nvlist for i in topology['cache']]
 
         if 'spare' in topology:
-            root.nvlist['spares'] = [(<ZFSVdev>i).nvlist for i in topology['cache']]
+            root.nvlist['spares'] = [(<ZFSVdev>i).nvlist for i in topology['spare']]
 
         if 'log' in topology:
             for i in topology['log']:
