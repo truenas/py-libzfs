9d9822db990b464e347f345ff8088d75b4fc68db
Alan Somers <asomers@gmail.com>
2020-01-10 15:21:51 -0700

    get_send_progress must be a method of ZFSSnapshot, not ZFSDataset
    ZFS_IOC_SEND_PROGRESS requires the name of the currently sending
    _snapshot_, not just the dataset.  That actually makes it fairly useless
    to consumers of zfs_send, which may send multiple snapshots in a row.

--- libzfs.pyx
+++ libzfs.pyx
@@ -3398,26 +3398,6 @@ cdef class ZFSDataset(ZFSResource):
         if err != 0:
             raise self.root.get_error()
 
-    def get_send_progress(self, fd):
-        IF HAVE_ZFS_IOCTL_HEADER:
-            cdef zfs.zfs_cmd_t cmd
-            memset(&cmd, 0, cython.sizeof(zfs.zfs_cmd_t))
-
-            cdef int ret
-
-            cmd.zc_cookie = fd
-            strncpy(cmd.zc_name, self.name, zfs.MAXPATHLEN)
-
-            with nogil:
-                ret = libzfs.zfs_ioctl(self.root.handle, zfs.ZFS_IOC_SEND_PROGRESS, &cmd)
-
-            if ret != 0:
-                raise ZFSException(Error.FAULT, "Cannot obtain send progress")
-
-            return cmd.zc_cookie
-        ELSE:
-            raise NotImplementedError()
-
     def promote(self):
         cdef int ret
 
@@ -3664,6 +3644,26 @@ cdef class ZFSSnapshot(ZFSResource):
             result = mntpt
             free(mntpt)
             return result
+
+    def get_send_progress(self, fd):
+        IF HAVE_ZFS_IOCTL_HEADER:
+            cdef zfs.zfs_cmd_t cmd
+            memset(&cmd, 0, cython.sizeof(zfs.zfs_cmd_t))
+
+            cdef int ret
+
+            cmd.zc_cookie = fd
+            strncpy(cmd.zc_name, self.name, zfs.MAXPATHLEN)
+
+            with nogil:
+                ret = libzfs.zfs_ioctl(self.root.handle, zfs.ZFS_IOC_SEND_PROGRESS, &cmd)
+
+            if ret != 0:
+                raise ZFSException(Error.FAULT, "Cannot obtain send progress")
+
+            return cmd.zc_cookie
+        ELSE:
+            raise NotImplementedError()
 
 cdef class ZFSBookmark(ZFSObject):
     def __getstate__(self):
