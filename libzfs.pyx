a575e8031787facfb41aeef84befaff3ce3e2296
Richard Kojedzinszky <richard@kojedz.in>
2022-01-13 21:57:11 +0100

    Free up nvlist objects where necessary

--- libzfs.pyx
+++ libzfs.pyx
@@ -449,7 +449,10 @@ cdef class ZFS(object):
                 # This is okay when non blocking behavior is desired
                 return None
             else:
-                return {'dropped': dropped, **dict(NVList(<uintptr_t>nvl))}
+                retval = {'dropped': dropped, **dict(NVList(<uintptr_t>nvl))}
+                with nogil:
+                    nvpair.nvlist_free(nvl)
+                return retval
 
     @staticmethod
     cdef int __iterate_props(int proptype, void *arg) nogil:
@@ -892,6 +895,9 @@ cdef class ZFS(object):
                     snap_data['holds'] = {}
                 else:
                     snap_data['holds'] = dict(NVList(<uintptr_t> ptr))
+
+            if ret == 0:
+                nvpair.nvlist_free(ptr)
 
         if mounted:
             ret = libzfs.zfs_is_mounted(handle, &mntpt)
@@ -1530,7 +1536,10 @@ cdef class ZFS(object):
             if nvl == NULL:
                 raise ZFSException(self.errno, self.errstr)
 
-            return dict(NVList(<uintptr_t>nvl))
+            retval = dict(NVList(<uintptr_t>nvl))
+            with nogil:
+                nvpair.nvlist_free(nvl)
+            return retval
 
 
 cdef class ZPoolProperty(object):
@@ -4005,8 +4014,10 @@ cdef class ZFSSnapshot(ZFSResource):
             if ret != 0:
                 raise self.root.get_error()
 
-            nvl = NVList(<uintptr_t>ptr)
-            return dict(nvl)
+            retval = dict(NVList(<uintptr_t>ptr))
+            with nogil:
+                nvpair.nvlist_free(ptr)
+            return retval
 
     property mountpoint:
         def __get__(self):
@@ -4163,8 +4174,10 @@ def read_label(device):
         raise OSError(errno.EINVAL, 'Cannot read label')
 
     os.close(fd)
-    nvlist = NVList(<uintptr_t>handle)
-    return dict(nvlist)
+    retval = dict(NVList(<uintptr_t>handle))
+    with nogil:
+        nvpair.nvlist_free(handle)
+    return retval
 
 
 def clear_label(device):
